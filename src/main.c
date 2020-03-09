24677978581f412b889366682528001606fc70bc
Stefano Bertoli <elkiwydev@gmail.com>
2020-03-09 21:59:41 +0100

    Added better command line arguments handling

--- src/main.c
+++ src/main.c
@@ -15,13 +15,40 @@
 
 
 int main(int argc, char** argv){
-	if (argc<2){
-		printf("Please specify a project root folder.\n");
-		return 0;
+
+	char* input  = NULL;
+	char* output = NULL;
+	char* style  = NULL;
+	char* mode   = NULL;
+
+	for (int i = 1; i < argc ; i++) {
+		if (argv[i][0] == '-'){
+			switch (argv[i][1]) {
+			case 'o': output = argv[i+1]; break;
+			case 'i': input = argv[i+1]; break;
+			case 's': style = argv[i+1]; break;
+			case 'm': mode = argv[i+1]; break;
+			default:
+				fprintf(stderr, "Usage: %s [-i projectRootFolder] [-m DEBUG/ORG/HTML] [-o outputFile] [-s cssFile]\n", argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}   
+
+	if (input==NULL || mode==NULL){
+		fprintf(stderr, "Please specify a input [-i rootFolder] and a mode [-m DEBUG/ORG/HTML]\n");
+		exit(EXIT_FAILURE);
 	}
 
-	Project* p = project_new("test", argv[1]);
-	render_project_debug(p);
-	render_project_org(p, "test_output.org");
+
+	Project* p = project_new("test", input);
+	if(strcmp(mode, "DEBUG")==0){
+		render_project_debug(p);
+	}else if(strcmp(mode, "ORG")==0){
+		render_project_org(p, "test_output.org");
+	}else{
+		fprintf(stderr, "Invalid mode '%s' must be one of: DEBUG, ORG, or HTML.\n", mode);
+		exit(EXIT_FAILURE);
+	}
 	return 0;
 }
