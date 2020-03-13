1bc4d50f70f3f237dbbdfd5cdfe1d6da1e61a267
elkiwy <elkiwydev@gmail.com>
2020-03-13 21:04:23 +0100

    Added test clojure file

---
+++ testProject/clojure.cljs
@@ -0,0 +1,24 @@
+
+;;;=Clojurescript module
+
+;;;~Test with no params
+(defn clojureFunctionNoParams []
+    )
+
+;;;~Test with one param
+(defn clojureFunctionOneParam [a]
+    )
+
+;;;~Test with multiple params
+(defn clojureFunctionTwoParam [a b]
+    )
+
+;;;~Test Docstring
+;;;&overridenName
+;;;#overriddenReturn
+;;;@1char k
+;;;@2char n
+(defn clojureFunctionTwoParamOverride [a b]
+    )
+
+
